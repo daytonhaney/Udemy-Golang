40ad8dbb217b13e967af019c2434002e50469e2c
Jay Pren <37848207+jpnamausa@users.noreply.github.com>
2021-12-29 14:58:30 -0500

    Add files via upload

---
+++ cards/deck.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+type deck []string // messing with this deck string he entire program, pretty much
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+//return two values both of type deck
+//inside of one function or instance
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		//if error #1 log the error and return call to new deck
+		// or - #2 log the error and shut program down
+		fmt.Println("Error!", err)
+		os.Exit(1)
+
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+
+	}
+}
